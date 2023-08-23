fb3bc5861fd4186c3ae3cc67e4e6a0934bf12647
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-08-23 12:51:45 +0330

    comment

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -13,7 +13,7 @@ from .serializers import (UserRegistrationSerializer,
                           ChangePasswordSerializer,
                         )
 from django.contrib.auth.hashers import check_password
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -97,21 +97,21 @@ class UserLoginView(APIView):
 #             return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-class UserLogoutView(APIView):
+# class UserLogoutView(APIView):
     
-    def post(self, request, *args, **kwargs):
-        refresh_token = request.data.get('refresh_token')
+#     def post(self, request, *args, **kwargs):
+#         refresh_token = request.data.get('refresh_token')
         
-        if not refresh_token:
-            return Response({"error": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+#         if not refresh_token:
+#             return Response({"error": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
         
-        try:
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            logout(request)
-            return Response({"message": "User logged out successfully."}, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({"error": "Invalid refresh token."}, status=status.HTTP_400_BAD_REQUEST)
+#         try:
+#             token = RefreshToken(refresh_token)
+#             token.blacklist()
+#             logout(request)
+#             return Response({"message": "User logged out successfully."}, status=status.HTTP_200_OK)
+#         except Exception as e:
+#             return Response({"error": "Invalid refresh token."}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserProfileView(APIView):
