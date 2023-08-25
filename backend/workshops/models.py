1e06bcc9a3e25eb802593758efee8d91a0fc9354
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-08-25 10:02:30 +0330

    add event crud

--- backend/workshops/models.py
+++ backend/workshops/models.py
@@ -20,4 +20,42 @@ class Event(SoftDeleteModel):
     def __str__(self):
         return self.title
     
-    
+# Create
+def create_event(title, description, location, capacity, price, image, start_event_at, end_event_at):
+    event = Event(
+        title=title,
+        description=description,
+        location=location,
+        capacity=capacity,
+        price=price,
+        image=image,
+        start_event_at=start_event_at,
+        end_event_at=end_event_at
+    )
+    event.save()
+    return event
+
+
+# Read
+def get_event(event_id):
+    try:
+        event = Event.objects.get(pk=event_id)
+        return event
+    except Event.DoesNotExist:
+        return None
+
+def get_all_events():
+    events = Event.objects.all()
+    return events
+
+
+# Update
+def update_event(event_id, **kwargs):
+    try:
+        event = Event.objects.get(pk=event_id)
+        for key, value in kwargs.items():
+            setattr(event, key, value)
+        event.save()
+        return event
+    except Event.DoesNotExist:
+        return None
