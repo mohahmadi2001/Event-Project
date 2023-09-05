ad1b1f849173066bbdf23f919e1b8e76e21859bc
Mohammad Mahdi <moh.ahmadi2001@yahoo.com>
2023-09-06 00:34:22 +0330

    remove id from url

--- backend/elections/urls.py
+++ backend/elections/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('', ElectionListView.as_view(), name='election_list'),
     path('register-candidate/', CandidateRegistrationView.as_view(), name='candidate-registration'),
     path('approved-candidates/', ApprovedCandidateListView.as_view(), name='approved-candidates-list'),
-    path('<int:election_id>/vote/', ElectionVoteView.as_view(), name='election-vote'),
-    path('<int:election_id>/results/', ElectionResultsView.as_view(), name='election-participant-count'),
+    path('vote/', ElectionVoteView.as_view(), name='election-vote'),
+    path('results/', ElectionResultsView.as_view(), name='election-participant-count'),
 ]
